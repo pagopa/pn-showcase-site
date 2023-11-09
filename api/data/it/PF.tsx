9b0de2beb07f63bd152ff75669a08c54b9dada46
salvatorediocaro <salvatore.diocaro@gmail.com>
2023-11-09 12:44:43 +0100

    [B2BP-156] - Implement changes to the "Cittadini" page
    The "cittadini" page content has been modified

--- api/data/it/PF.tsx
+++ api/data/it/PF.tsx
@@ -22,18 +22,19 @@ const onReadClick = () => {
 // eslint-disable-next-line no-extra-boolean-cast
 const heroCta = !!PN_PF_URL
   ? {
-    label: "Leggi le tue notifiche",
-    title: "Leggi le tue notifiche",
-    onClick: onReadClick,
+    label: "Accedi a SEND",
+    title: "Accedi a SEND",
+    onClick: function onClick() {
+      window.open(PN_PF_URL, "_self");
+    },
   }
   : undefined;
 
-const heroSubtitle = `Con SEND - Servizio Notifiche Digitali (anche nota come Piattaforma Notifiche Digitali di cui all'art. 26 del decreto-legge 76/2020 s.m.i.) puoi ricevere istantaneamente le comunicazioni a valore legale da parte di un ente. 
-Potrai visualizzarle, gestirle e pagarle direttamente online sulla piattaforma dedicata o dall'app IO.`;
+const heroSubtitle = `Con SEND - Servizio Notifiche Digitali (anche nota come Piattaforma Notifiche Digitali di cui all'art. 26 del decreto-legge 76/2020 s.m.i.) puoi ricevere istantaneamente le comunicazioni a valore legale da parte di un ente. Potrai visualizzarle, gestirle e pagarle direttamente online sulla piattaforma o dall'app IO.`;
 
 export const pfHero: HeroProps = {
   type: "image",
-  title: "Le notifiche? Sono a portata di mano.",
+  title: "Le notifiche? Sono a portata di mano",
   subtitle: (
     <Typography
       component="p"
@@ -56,42 +57,25 @@ export const pfHero: HeroProps = {
 /* ************************************** */
 
 /** Infoblocks mocked data */
-const infoblock1_1 = `Le notifiche sono comunicazioni a valore legale emesse in via
-ufficiale da un'amministrazione pubblica, come esiti di pratiche
-amministrative o rimborsi, multe e avvisi di accertamento di
-tributi. Da oggi puoi riceverle e consultarle in digitale, accedendo
-online a SEND - Servizio Notifiche Digitali tramite SPID o CIE o
-direttamente da app IO.`;
+const infoblock1_1 = `Le notifiche sono comunicazioni a valore legale emesse in via ufficiale da un'amministrazione pubblica, come esiti di pratiche amministrative o rimborsi, multe e avvisi di accertamento di tributi. Da oggi puoi riceverle e consultarle in digitale, accedendo online a SEND - Servizio Notifiche Digitali tramite SPID o CIE o direttamente dall’app IO.`;
 
-const infoblock1_2 = `Puoi anche pagare eventuali costi grazie all'integrazione con
-pagoPA, visualizzare lo storico delle notifiche ricevute e gestirle
-direttamente online. Inoltre, ti basta accettare una delega per
-accedere anche alle notifiche dei tuoi familiari.`;
+const infoblock1_2 = `Puoi anche pagare eventuali costi grazie all'integrazione con pagoPA, visualizzare lo storico delle notifiche ricevute e gestirle direttamente online. Inoltre, ti basta accettare una delega per accedere anche alle notifiche dei tuoi familiari.`;
 
-const infoblock2 = `Per inviare le comunicazioni a valore legale, SEND dà sempre la
-priorità ai recapiti digitali del destinatario. In ogni momento,
-puoi accedere online al Servizio Notifiche Digitali con SPID e CIE
-per indicare o aggiornare le tue preferenze tra PEC, App IO, email
-e/o numero di cellulare . Se non indichi alcun recapito o non accedi
-alla notifica attraverso SEND da canali diversi dalla PEC entro i
-tempi di seguito indicati, continuerai a ricevere le notifiche
-tramite raccomandata cartacea.`;
+const infoblock2 = `Per inviarti le comunicazioni a valore legale, SEND dà la priorità ai tuoi recapiti digitali. In ogni momento, puoi accedere online al Servizio Notifiche Digitali con SPID e CIE per indicare o aggiornare o il tuo recapito legale (PEC) oppure i tuo recapiti di cortesia (app IO, email e/o numero di cellulare). Se non indichi alcun recapito o non accedi alla notifica attraverso SEND da canali diversi dalla PEC `;
+
+const infoblock2_1 = `entro i tempi indicati`;
+
+const infoblock2_2 = `, continuerai a ricevere le notifiche tramite raccomandata cartacea.`;
 
 const infoblock3_1 = `SEND è a disposizione di tutte le Pubbliche
 Amministrazioni che vorranno utilizzarlo per inviare notifiche ai
 destinatari delle loro comunicazioni a valore legale.`;
 
-const infoblock3_2 = `Il servizio sarà adottato dagli enti progressivamente e, per
-favorirne la graduale diffusione tra i cittadini, in una prima fase
-assicurerà l'invio anche della copia analogica conforme degli atti
-notificati tramite raccomandata cartacea ai destinatari non dotati
-di domicilio digitale.`;
+const infoblock3_2 = `Il servizio sarà adottato dagli enti progressivamente e, per favorirne la graduale diffusione tra i cittadini, in una prima fase assicurerà l'invio anche della copia analogica conforme degli atti notificati tramite raccomandata cartacea ai destinatari non dotati di recapito legale (PEC).`;
 
 const infoblock3_3 = `Il futuro delle comunicazioni a valore legale`;
 
-const infoblock3_4 = `SEND è a disposizione di tutte le Pubbliche Amministrazioni che
-vorranno utilizzarlo per inviare notifiche ai destinatari delle loro
-comunicazioni a valore legale.`;
+const infoblock3_4 = `SEND è a disposizione di tutti gli enti che vorranno utilizzarlo per inviare notifiche ai destinatari delle loro comunicazioni a valore legale.`;
 
 export const pfInfoBlocks: Array<IInfoblockData> = [
   {
@@ -123,7 +107,17 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
       content: (
         <>
           <Typography variant="body2" tabIndex={0} aria-label={infoblock2}>
-            {infoblock2}
+              {infoblock2}
+              <Link
+                href="/perfezionamento"
+                // target="_blank"
+                rel="noopener noreferrer"
+                aria-label={infoblock2_1}
+                sx={{fontWeight: "bold"}}
+              >
+                {infoblock2_1}
+              </Link>
+              {infoblock2_2}
           </Typography>
         </>
       ),
@@ -141,13 +135,13 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
       title: "",
       content: (
         <>
-          <Typography variant="h4" tabIndex={0} aria-label={infoblock3_3} sx={{color: "primary.contrastText"}}>
+          <Typography variant="h4" tabIndex={0} aria-label={infoblock3_3} sx={{ color: "primary.contrastText" }}>
             {infoblock3_3}
           </Typography>
-          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_4} sx={{color: "primary.contrastText"}}>
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_4} sx={{ color: "primary.contrastText" }}>
             {infoblock3_4}
           </Typography>
-          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_2} sx={{color: "primary.contrastText"}}>
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_2} sx={{ color: "primary.contrastText" }}>
             {infoblock3_2}
           </Typography>
         </>
@@ -164,38 +158,25 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
 
 /** Showcase mocked data */
 const showcase1_1 =
-  "Il recapito delle notifiche in digitale comporta ";
+  "Il recapito delle notifiche in digitale comporta minori costi di notificazione e spedizione";
 const showcase1_2 =
   "Niente più attese o code per il ritiro delle comunicazioni cartacee";
 const showcase1_3 =
   "Contribuisci a ridurre il consumo di carta e le emissioni per il trasporto";
 const showcase1_4 =
   "Non devi più conservare i documenti stampati, grazie alla possibilità di scaricare e archiviare gli atti in digitale";
-const showcase1_5 =
-  "minori costi di notificazione e spedizione";
 
-const showcase2_1 = `Se hai un indirizzo PEC, le notifiche ti risulteranno legalmente
-  consegnate, senza più raccomandate cartacee. L'avviso di avvenuta
-  ricezione che ti sarà inviato contiene il link per accedere al
-  contenuto su SEND.`;
-const showcase2_2 = `Se attivi il servizio “Notifiche digitali” di SEND, puoi ricevere un avviso di cortesia e 
-  gestire direttamente in app le comunicazioni a valore legale. Se
-  non hai la PEC ma accedi alla notifica attraverso SEND dall'app e
-  leggi la notifica entro 5 giorni (120 ore) dalla sua ricezione,
-  questa ti risulterà legalmente recapitata e non riceverai alcuna
-  raccomandata cartacea.`;
-const showcase2_3 = `In più, puoi anche scegliere di ricevere un avviso di cortesia al
-  tuo indirizzo e-mail o tramite SMS. Se non hai la PEC ma accedi
-  alla notifica attraverso SEND dall'apposito link entro 5 giorni
-  (120 ore) dalla ricezione della notifica, questa ti risulterà
-  legalmente recapitata e non riceverai alcuna raccomandata
-  cartacea.`;
+const showcase2_1 = `Se hai indicato un indirizzo PEC, le notifiche risulteranno legalmente consegnate, senza più raccomandate cartacee. L'avviso di avvenuta ricezione che ti sarà inviato contiene il link per accedere ai documenti su SEND.`;
+
+const showcase2_2 = `Se attivi il servizio “SEND - Notifiche digitali”, puoi ricevere un avviso di cortesia e gestire direttamente in app le comunicazioni a valore legale. Se non hai un indirizzo PEC ma accedi alla notifica dall'app e leggi la notifica entro 5 giorni (120 ore) dalla sua ricezione, questa risulterà legalmente recapitata e non riceverai alcuna raccomandata cartacea.`;
+
+const showcase2_3 = `In più, puoi anche scegliere di ricevere un avviso di cortesia al tuo indirizzo e-mail o tramite SMS. Se non hai la PEC ma accedi alla notifica attraverso SEND dall'apposito link entro 5 giorni (120 ore) dalla ricezione della notifica, questa risulterà legalmente recapitata e non riceverai alcuna raccomandata cartacea.`;
 
 export const pfShowcases: Array<IShowcaseData> = [
   {
     name: "showcase 1",
     data: {
-      title: "Cosa ti offrono le notifiche digitali",
+      title: "Un solo modo per risparmiare in tanti modi",
       items: [
         {
           icon: <PiggyIcon />,
@@ -203,17 +184,7 @@ export const pfShowcases: Array<IShowcaseData> = [
           subtitle: (
             <Typography variant="body2" tabIndex={0} aria-label={showcase1_1}>
               {showcase1_1}
-              <Link
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={showcase1_5}
-                sx={{fontWeight: "bold"}}
-              >
-                {showcase1_5}
-              </Link>
             </Typography>
-
           ),
         },
         {
@@ -295,26 +266,18 @@ di avvenuta ricezione. Invia anche un avviso di cortesia agli altri tuoi recapit
 (app IO, e-mail e numero di cellulare), se li hai inseriti. Se non hai indicato alcun recapito digitale 
 e non accedi online  alla notifica attraverso SEND, riceverai una raccomandata cartacea.
 `;
-const walkthrough2 = `
-Dal messaggio ricevuto, puoi accedere online alla piattaforma per leggere la notifica e scaricare i relativi 
-documenti allegati. Se attivi il servizio su IO, puoi visualizzare il contenuto direttamente in app: questo 
-equivale alla firma della ricevuta di ritorno di una raccomandata tradizionale e al
-`;
+const walkthrough2 = `Dal messaggio ricevuto, puoi accedere online alla piattaforma per leggere la notifica e scaricare i relativi documenti allegati. Se attivi il servizio su IO, puoi visualizzare il contenuto direttamente in app: questo equivale alla firma della ricevuta di ritorno di una raccomandata tradizionale e al `;
+
 const walkthrough2_1 = `
  immediato della notifica.
 `;
-const walkthrough2_2 = `
- perfezionamento
-`;
+const walkthrough2_2 = `perfezionamento`;
 const walkthrough3 = `
 Se c'è un importo da pagare, grazie all'integrazione con pagoPA, puoi procedere contestualmente online da SEND 
 oppure direttamente da IO. Se preferisci recarti presso uno sportello, dovrai avere con te il modulo di pagamento 
 allegato alla notifica.
 `;
-const walkthrough4 = `
-Se lo desideri, puoi delegare altre persone, fisiche o giuridiche, a ricevere le tue notifiche online. 
-Per farlo, accedi a SEND con SPID o CIE e inserisci nella sezione Deleghe i dati della persona che vuoi delegare.
-`;
+const walkthrough4 = `Se lo desideri, puoi delegare altre persone, fisiche o giuridiche, a visualizzare le tue notifiche online. Per farlo, accedi a SEND con SPID o CIE e inserisci nella sezione Deleghe i dati della persona che vuoi delegare.`;
 export const pfWalkthrough: WalkthroughProps = {
   title: "Come funziona?",
   items: [
@@ -339,7 +302,7 @@ export const pfWalkthrough: WalkthroughProps = {
               target="_blank"
               rel="noopener noreferrer"
               aria-label={walkthrough2_2}
-              sx={{fontWeight: "bold"}}
+              sx={{ fontWeight: "bold" }}
             >
               {walkthrough2_2}
             </Link>
