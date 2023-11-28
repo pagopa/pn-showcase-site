263807beb7415d095e3968b77a14dbc1b229b55b
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2023-11-28 18:09:32 +0100

    chore: removed useless anchor

--- pages/pubbliche-amministrazioni/index.tsx
+++ pages/pubbliche-amministrazioni/index.tsx
@@ -27,10 +27,6 @@ const EntiPage: NextPage = () => (
       <Infoblock {...getInfoblockData(USER_TYPE, "infoblock 3")}></Infoblock>
       <Showcase {...getShowcaseData(USER_TYPE, "showcase 1")} />
       <div className="lightWalkthrough"><Walkthrough {...getWalkthroughData(USER_TYPE)} /></div>  
-      {/* Carlotta Dimatteo - workaround per gestire un anchor interno alla pagina richiesto dal team di comunicazione il 16/02/2023  */}
-      <div id="start-integration">
-        <></>
-      </div>
       <StripeLinkEnti></StripeLinkEnti>
       <DarkInfoblockEnti></DarkInfoblockEnti>
     </main>
