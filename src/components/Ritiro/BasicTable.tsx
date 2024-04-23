6cd2ef089802e509e00f4f1963a849d4ac53257a
Sarah Donvito <sarah.donvito@pagopa.it>
2024-04-23 15:00:18 +0200

    feat(PN-10729): add table to page punti-di-ritiro

--- src/components/Ritiro/BasicTable.tsx
+++ src/components/Ritiro/BasicTable.tsx
@@ -1,56 +1,70 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import Paper from "@mui/material/Paper";
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
+import { visuallyHidden } from "@mui/utils";
+interface Data {
+  denomination: string;
+  region: string;
+  city: string;
+  address: string;
+  contacts: string;
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+export default function BasicTable({ parsedData }) {
+  function createData(
+    denomination: string,
+    region: string,
+    city: string,
+    address: string,
+    contacts: string
+  ) {
+    return { denomination, region, city, address, contacts };
+  }
 
-export default function BasicTable() {
+  let rows: Data[] = [];
+  parsedData.map(
+    (e: {
+      descrizione: string;
+      citta: string;
+      via: string;
+      Telefono: string;
+    }) => {
+      rows.push(createData(e.descrizione, "N.D.", e.citta, e.via, e.Telefono));
+    }
+  );
+  const keys = ["Denominazione", "Regione", "Città", "Indirizzo", "Contatti"];
+
   return (
     <TableContainer component={Paper}>
-      <Table aria-label="punti di ritiro SEND">
-        <TableHead sx={{ backgroundColor: "#FAFAFA" }}>
+      <Table sx={{ width: "100%", maxWidth: 1092 }} aria-label="simple table">
+        <TableHead>
           <TableRow>
-            <TableCell>Denominazione</TableCell>
-            <TableCell align="right">Regione</TableCell>
-            <TableCell align="right">Città</TableCell>
-            <TableCell align="right">Indirizzo</TableCell>
-            <TableCell align="right">Contatti</TableCell>
+            {keys.map((name: string) => (
+              <TableCell>{name}</TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.denomination}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.denomination}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.region}</TableCell>
+              <TableCell>{row.city}</TableCell>
+              <TableCell>{row.address}</TableCell>
+              <TableCell>{row.contacts}</TableCell>
             </TableRow>
           ))}
         </TableBody>
