b0e3e7bd2fb2e1d203ed1eacfcfba16a3a16f9c0
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2024-07-24 11:43:01 +0200

    fix(PN-12162): add redirect from /documenti to /pubblica-amministrazione/documenti

---
+++ pages/documenti.tsx
@@ -0,0 +1,14 @@
+// pages/documenti.js
+import Head from 'next/head';
+
+const Documenti = () => {
+    return (
+        <>
+            <Head>
+                <meta http-equiv="refresh" content="0; url=/pubbliche-amministrazioni/documenti" />
+            </Head>
+        </>
+    );
+};
+
+export default Documenti;
