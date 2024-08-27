da136f7d9425f2ac111c216cc804bc7aaff434f9
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2024-08-27 12:52:31 +0200

    refactor: apply translation to 'perfezionamento' section refactor: add CustomInfoblock Content, List and ListItem to reduce code duplication and increase readability refactor: delete api/data/perfezionamento module used to store content before applying translation changes

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,27 +1,4 @@
-import { InfoblockProps } from "@pagopa/mui-italia";
-
-import { ITabsProps, IFaqData, IHeadingTitleProps } from "../model";
-
+import { IFaqData } from "../model";
 import { itFaqData } from "./data/faq-it";
-import { perfezionamentoData } from "./data/perfezionamento";
 
 export const getFaqData = (): IFaqData => itFaqData;
-
-export const getCommonHeadingTitleData = (name: string): IHeadingTitleProps => {
-  const headingTitleData = perfezionamentoData.headingTitles.filter(
-    (f) => f.name === name
-  )[0];
-  return headingTitleData.data;
-};
-
-export const getCommonTabsData = (name: string): ITabsProps => {
-  const tabsData = perfezionamentoData.tabs.filter((f) => f.name === name)[0];
-  return tabsData.data;
-};
-
-export const getCommonInfoblockData = (name: string): InfoblockProps => {
-  const infoblockData = perfezionamentoData.infoblocks.filter(
-    (f) => f.name === name
-  )[0];
-  return infoblockData.data;
-};
