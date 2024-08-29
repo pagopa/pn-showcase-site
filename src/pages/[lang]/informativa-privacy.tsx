33b308c2b798ad3166d653e5e597941eb47fb1f1
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2024-08-29 16:35:56 +0200

    feat(pn 12188, 12189, 12190) - Apply translation to pages 'Assistenza', 'Privacy', 'Note legali' and 'Perfezionamento' (#81)

--- src/pages/[lang]/informativa-privacy.tsx
+++ src/pages/[lang]/informativa-privacy.tsx
@@ -7,6 +7,7 @@ import { langCodes, ONE_TRUST_PP_PAGE } from "@utils/constants";
 import PageHead from "../../components/PageHead";
 import { LangCode } from "../../model";
 import { getI18n } from "../../api/i18n";
+import { useTranslation } from "../../hook/useTranslation";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
@@ -14,13 +15,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
       params: { lang },
     })),
     fallback: false,
-  }
-}
+  };
+};
 
-export async function getStaticProps({params}: {params: {lang: LangCode}}) {
-  const translations = getI18n(params.lang, ['common'])
+export async function getStaticProps({ params }: { params: { lang: LangCode } }) {
+  const translations = getI18n(params.lang, ["common", "privacy"]);
 
-  return { props: {translations, lang: params.lang} }
+  return { props: { translations, lang: params.lang } };
 }
 
 declare const OneTrust: {
@@ -32,32 +33,29 @@ declare const OneTrust: {
   };
 };
 
+const loadOneTrust = () => {
+  if (typeof OneTrust !== "undefined" && ONE_TRUST_PP_PAGE) {
+    OneTrust.NoticeApi.Initialized.then(() => {
+      OneTrust.NoticeApi.LoadNotices([ONE_TRUST_PP_PAGE], false);
+    });
+  }
+};
+
 const PrivacyPage: NextPage = () => {
-  useEffect(() => {
-    if (ONE_TRUST_PP_PAGE) {
-      OneTrust.NoticeApi.Initialized.then(() => {
-        OneTrust.NoticeApi.LoadNotices([ONE_TRUST_PP_PAGE], false);
-      });
-    }
-  }, []);
+  const { t } = useTranslation(["common", "privacy"]);
 
   return (
     <>
-      <PageHead
-        title="SEND - Servizio Notifiche Digitali | Informativa Privacy"
-        description="Consulta l'informativa sul trattamento dei dati personali di SEND - Servizio Notifiche digitali"
-      />
+      <PageHead title={t("title", { ns: "privacy" })} description={t("description", { ns: "privacy" })} />
       <Script
         src="/onetrust/privacy-notice-scripts/otnotice-1.0.min.js"
         type="text/javascript"
         charSet="UTF-8"
         id="otprivacy-notice-script"
         strategy="beforeInteractive"
+        onReady={() => loadOneTrust()}
       />
-      <div
-        id="otnotice-b5c8e1dc-89df-4ec2-a02d-1c0f55fac052"
-        className={`otnotice`}
-      ></div>
+      <div id="otnotice-b5c8e1dc-89df-4ec2-a02d-1c0f55fac052" className={`otnotice`}></div>
     </>
   );
 };
