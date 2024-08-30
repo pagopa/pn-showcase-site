3a4cb4b8e2ea31b1f05354d13dda4aabcb2ec37a
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2024-08-30 12:49:35 +0200

    feat(pn-12484): Code optimization (#83)

--- src/context/lang-context.tsx
+++ src/context/lang-context.tsx
@@ -12,7 +12,7 @@ interface ILangContext {
 const LangContext = createContext<ILangContext>({
   lang: DEFAULT_LANG,
   changeLanguage: (lang: LangCode) => {},
-  translations: {}
+  translations: {},
 });
 
 interface Props {
@@ -21,41 +21,42 @@ interface Props {
   translations: I18n;
 }
 
-export const LangProvider: React.FC<Props> = ({ children, lang =  DEFAULT_LANG, translations}) => {
-  const [selectedLang, setSelectedLang] = useState<LangCode>(langCodes.find(l => l === lang) ?? DEFAULT_LANG);
+export const LangProvider: React.FC<Props> = ({ children, lang = DEFAULT_LANG, translations }) => {
+  const [selectedLang, setSelectedLang] = useState<LangCode>(langCodes.find((l) => l === lang) ?? DEFAULT_LANG);
   const router = useRouter();
   const { pathname, query } = router;
 
-  const changeLanguageHandler = useCallback((newLang: LangCode) => {
-    sessionStorage.setItem(LS_LANG_PROP_NAME, newLang);
-    setSelectedLang(() => newLang);
-    // redirect to page
-    // the reload is needed because the _document is rendered server side and
-    // it isn't re-rendered when changes occur on client side. This means that the lang
-    // attribute isn't changed on router navigation, but only on refresh
-    router.replace({ pathname, query: {lang: newLang} }, undefined, {shallow: true})
-      .then(() => router.reload());
-  }, [pathname, query]);
+  const changeLanguageHandler = useCallback(
+    (newLang: LangCode) => {
+      sessionStorage.setItem(LS_LANG_PROP_NAME, newLang);
+      setSelectedLang(() => newLang);
+      // redirect to page
+      // the reload is needed because the _document is rendered server side and
+      // it isn't re-rendered when changes occur on client side. This means that the lang
+      // attribute isn't changed on router navigation, but only on refresh
+      router.replace({ pathname, query: { lang: newLang } }, undefined, { shallow: true }).then(() => router.reload());
+    },
+    [pathname, query]
+  );
 
   // Sync context with router
   useEffect(() => {
     if (query.lang && langCodes.includes(query.lang as LangCode) && lang !== query.lang) {
-      setSelectedLang(query.lang  as LangCode);
+      setSelectedLang(query.lang as LangCode);
       sessionStorage.setItem(LS_LANG_PROP_NAME, query.lang as string);
     }
   }, [query.lang, lang]);
 
-  return (
-    <LangContext.Provider
-      value={{
-        lang: selectedLang,
-        changeLanguage: changeLanguageHandler,
-        translations
-      }}
-    >
-      {children}
-    </LangContext.Provider>
+  const obj = useMemo(
+    () => ({
+      lang: selectedLang,
+      changeLanguage: changeLanguageHandler,
+      translations,
+    }),
+    [selectedLang, changeLanguageHandler, translations]
   );
+
+  return <LangContext.Provider value={obj}>{children}</LangContext.Provider>;
 };
 
 export default LangContext;
