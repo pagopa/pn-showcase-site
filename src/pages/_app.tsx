851154352cf5d0e25c438ced16770c6bde9c824c
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2024-09-03 17:57:45 +0200

    feat(pn-12440): Avoid showing untranslated labels during translations loading (#85)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,26 @@ import Layout from "../components/Layout";
 
 import "../styles/default.css";
 import { LangProvider } from "../context/lang-context";
+import Loading from "../components/loading";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const noLayout = pageProps.noLayout;
+  const translationLoading = !pageProps.lang && !pageProps.translations;
 
   return (
     <ThemeProvider theme={theme}>
       <LangProvider lang={pageProps.lang} translations={pageProps.translations}>
+        
+        {translationLoading && <Loading />}
+        
         {noLayout ? (
           // Se noLayout è true, renderizza solo il componente
-          <Component {...pageProps} />
+            <Component {...pageProps} />
         ) : (
           // Altrimenti, avvolgilo nel LandingLayout
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
         )}
       </LangProvider>
     </ThemeProvider>
