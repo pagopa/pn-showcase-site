84cb3426ba452c153866665368ff532b5739ed76
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2024-09-03 15:46:09 +0200

    chore: remove useless bgColor and zIndex properties from Loading Skeletons

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -4,19 +4,19 @@ const Loading = () => {
   return (
     <Stack sx={{ background: 'white'}} position="absolute" zIndex={1000} height="100%" width="100%" direction="column">
       <Skeleton // Header
-        sx={{ height: '20%', mb: 3, backgroundColor: "background.default", zIndex: 1000 }}
+        sx={{ height: '20%', mb: 3 }}
         width="100%"
         animation="wave"
         variant="rounded"
       />
       <Skeleton // Main
-        sx={{ height: '50%', mb: 3, backgroundColor: "background.default", zIndex: 1000 }}
+        sx={{ height: '50%', mb: 3 }}
         width="100%"
         animation="wave"
         variant="rounded"
       />
       <Skeleton // Footer
-        sx={{ height: '25%', backgroundColor: "background.default", zIndex: 1000 }}
+        sx={{ height: '25%' }}
         width="100%"
         animation="wave"
         variant="rounded"
