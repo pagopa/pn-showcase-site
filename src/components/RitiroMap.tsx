18bdd0ded53ab44b990373b39574ab1d2c4bb965
alessandrogelmi <ale.gelmi2@gmail.com>
2025-04-14 17:06:19 +0200

    feat: use user location

--- src/components/RitiroMap.tsx
+++ src/components/RitiroMap.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import L from "leaflet";
-import React, { Fragment } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { RaddOperator } from "../model";
 import MarkerCluster from "./Map/ClusterMarker";
 
 type Props = {
   points: Array<RaddOperator>;
 };
+
+function MapController({
+  userLocation,
+}: {
+  userLocation: { latitude: number; longitude: number } | null;
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (userLocation) {
+      map.flyTo([userLocation.latitude, userLocation.longitude], 10);
+    }
+  }, [userLocation, map]);
+
+  return null;
+}
 
 const getIcon = (iconUrl: string) => {
   return L.icon({
@@ -20,38 +36,59 @@ const getIcon = (iconUrl: string) => {
 };
 
 const PickupPointsMap: React.FC<Props> = ({ points }) => {
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  const getUserLocation = async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log("Got coordinates", latitude, longitude);
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  const validPoints = points.filter(
+    (point) => point.latitude && point.longitude
+  );
+
+  const markers = validPoints.map((p) => ({
+    position: { lat: p.latitude || 0, lng: p.longitude || 0 },
+    text: p.address,
+  }));
+
   return (
     <MapContainer
-      center={[41.8719, 12.5674]}
-      zoom={6}
+      center={
+        userLocation
+          ? [userLocation.latitude, userLocation.longitude]
+          : [41.8719, 12.5674]
+      }
+      zoom={userLocation ? 10 : 6}
       style={{ height: "100%", width: "100%" }}
     >
+      <MapController userLocation={userLocation} />
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {points.map((point) => {
-        if (point.latitude && point.longitude) {
-          return (
-            <Fragment key={`${point.denomination}-${point.address}`}>
-              {/* <Marker
-                position={[point.latitude, point.longitude]}
-                icon={getIcon("/static/images/pointer.svg")}
-              >
-                <Popup closeButton={true}>{point.address}</Popup>
-              </Marker> */}
-              <MarkerCluster
-                markers={points.map((p) => {
-                  return {
-                    position: { lat: p.latitude || 0, lng: p.longitude || 0 },
-                    text: p.address,
-                  };
-                })}
-              />
-            </Fragment>
-          );
-        }
-      })}
+
+      {markers.length > 0 && <MarkerCluster markers={markers} />}
     </MapContainer>
   );
 };
