d8a0338e5976de7de0af8837db59bf6496f1e578
Francesco Bianchi <f.bianchi@codermine.com>
2025-06-05 13:59:29 +0200

    button load more

--- src/components/PickupPointsList/index.tsx
+++ src/components/PickupPointsList/index.tsx
@@ -1,15 +1,19 @@
 import { Place } from "@mui/icons-material";
 import { Box, List, ListItem, Paper, Stack, Typography } from "@mui/material";
 import { ButtonNaked } from "@pagopa/mui-italia";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { RaddOperator } from "../../model";
 import { useTranslation } from "../../hook/useTranslation";
+import { Refresh } from "@mui/icons-material";
+
+const PAGE_SIZE = 5;
 
 type Props = {
   rows: RaddOperator[];
 };
 
 function PickupPointsList({ rows }: Readonly<Props>) {
+  const [numberOfRows, setNumberOfRows] = useState(PAGE_SIZE);
   const { t } = useTranslation(["pickup"]);
 
   const listContainerRef = useRef<HTMLUListElement | null>(null);
@@ -19,67 +23,96 @@ function PickupPointsList({ rows }: Readonly<Props>) {
   };
 
   return (
-    <List
-      ref={listContainerRef}
-      sx={{ maxHeight: "800px", overflowY: "auto" }}
-      aria-live="polite"
-    >
-      {rows.slice(0, 5).map((row, index: number) => (
-        <ListItem
-          key={`${row.denomination}-${index}`}
-          sx={{
-            border: "1px solid",
-            borderColor: "divider",
-            borderRadius: "8px",
-            my: 2,
-            p: 3,
-          }}
-        >
-          <Stack component={Paper} width="100%">
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="flex-start"
+    <>
+      <List
+        ref={listContainerRef}
+        sx={{ maxHeight: "800px", overflowY: "auto" }}
+        aria-live="polite"
+      >
+        {rows.slice(0, numberOfRows).map((row, index: number) => (
+          <ListItem
+            key={`${row.denomination}-${index}`}
+            sx={{
+              border: "1px solid",
+              borderColor: "divider",
+              borderRadius: "8px",
+              my: 2,
+              p: 3,
+            }}
+          >
+            <Stack
+              component={Paper}
+              width="100%"
             >
-              <Box mb={1}>
-                <Typography variant="body1" fontWeight={600}>
-                  {row.denomination}
-                </Typography>
-                <Typography variant="body2" fontSize="14px">
-                  {row.address}, {row.city} ({row.province})
-                </Typography>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="flex-start"
+              >
+                <Box mb={1}>
+                  <Typography
+                    variant="body1"
+                    fontWeight={600}
+                  >
+                    {row.denomination}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontSize="14px"
+                  >
+                    {row.address}, {row.city} ({row.province})
+                  </Typography>
+                </Box>
+
+                {row.distance && (
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    alignItems="center"
+                    justifyContent="flex-end"
+                  >
+                    <Place
+                      fontSize="small"
+                      sx={{ color: "text.secondary" }}
+                    />
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      km {row.distance?.toFixed(1) || "-"}
+                    </Typography>
+                  </Stack>
+                )}
               </Box>
 
-              {row.distance && (
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  alignItems="center"
-                  justifyContent="flex-end"
-                >
-                  <Place fontSize="small" sx={{ color: "text.secondary" }} />
-                  <Typography variant="body2" color="text.secondary">
-                    km {row.distance?.toFixed(1) || "-"}
-                  </Typography>
-                </Stack>
-              )}
-            </Box>
+              <ButtonNaked
+                color="primary"
+                sx={{
+                  justifyContent: "flex-start",
+                  width: "fit-content",
+                  alignItems: "center",
+                }}
+                onClick={() => onShowDetailsClick(row)}
+              >
+                {t("show-details")}
+              </ButtonNaked>
+            </Stack>
+          </ListItem>
+        ))}
 
-            <ButtonNaked
-              color="primary"
-              sx={{
-                justifyContent: "flex-start",
-                width: "fit-content",
-                alignItems: "center",
-              }}
-              onClick={() => onShowDetailsClick(row)}
-            >
-              {t("show-details")}
-            </ButtonNaked>
-          </Stack>
-        </ListItem>
-      ))}
-    </List>
+        <Box sx={{ display: "flex", justifyContent: "center", my: 2 }}>
+          <ButtonNaked
+            color="primary"
+            onClick={() => {
+              setNumberOfRows((prev) => prev + PAGE_SIZE);
+            }}
+            startIcon={<Refresh />}
+          >
+            {t("show-more")}
+          </ButtonNaked>
+        </Box>
+      </List>
+    </>
   );
 }
 
