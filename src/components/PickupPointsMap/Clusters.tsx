fd735977b6b84d30f948a618239a83c1f5fca22f
alessandrogelmi <ale.gelmi2@gmail.com>
2025-06-05 17:34:51 +0200

    feat: render points with clusters

---
+++ src/components/PickupPointsMap/Clusters.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Layer, Source } from "react-map-gl/maplibre";
+import { RaddOperator } from "src/model";
+import {
+  clusterCountLayer,
+  clusterLayer,
+  unclusteredPointLayer,
+} from "./layers";
+
+interface RegionClusterProps {
+  points: RaddOperator[];
+}
+
+const Clusters: React.FC<RegionClusterProps> = ({ points }) => {
+  const geojsonData: GeoJSON.GeoJSON = {
+    type: "FeatureCollection",
+    features: points.map((point) => ({
+      type: "Feature",
+      properties: {
+        name: point.denomination,
+        address: point.address,
+        region: point.region,
+        province: point.province,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [point.longitude, point.latitude],
+      },
+    })),
+  };
+
+  return (
+    <Source
+      id="stores"
+      type="geojson"
+      data={geojsonData}
+      cluster
+      clusterMaxZoom={12}
+      clusterRadius={50}
+    >
+      <Layer {...clusterLayer} />
+      <Layer {...clusterCountLayer} />
+      <Layer {...unclusteredPointLayer} />
+    </Source>
+  );
+};
+
+export default Clusters;
--- src/components/PickupPointsMap/index.tsx
+++ src/components/PickupPointsMap/index.tsx
@@ -1,8 +1,10 @@
+import { MapLibreEvent } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { Map } from "react-map-gl/maplibre";
 import { Coordinates, RaddOperator } from "src/model";
 import MapControls from "./MapControls";
 import UserPositionController from "./UserPositionController";
+import Clusters from "./Clusters";
 
 type Props = {
   points: Array<RaddOperator>;
@@ -12,6 +14,12 @@ type Props = {
 const PickupPointsMap: React.FC<Props> = ({ points, userPosition }) => {
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
+  const handleLoad = async (event: MapLibreEvent) => {
+    const map = event.target;
+    const response = await map.loadImage("/static/images/map/base-marker.png");
+    map.addImage("base-marker", response.data);
+  };
+
   return (
     <Map
       mapStyle={`https://maps.geo.eu-central-1.amazonaws.com/v2/styles/Standard/descriptor?key=${API_KEY}`}
@@ -20,11 +28,14 @@ const PickupPointsMap: React.FC<Props> = ({ points, userPosition }) => {
         latitude: 41.895679,
         zoom: 10,
       }}
+      minZoom={5}
+      onLoad={handleLoad}
+      reuseMaps
       style={{ height: "100%", width: "100%", position: "relative" }}
     >
       <UserPositionController points={points} userPosition={userPosition} />
-
       <MapControls userPosition={userPosition} />
+      <Clusters points={points} />
     </Map>
   );
 };
