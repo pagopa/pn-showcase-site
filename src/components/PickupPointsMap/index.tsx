1e0227182e38bad162ac0aa76049a4023d66a045
Alessandro Gelmi <ale.gelmi2@gmail.com>
2025-06-17 15:35:05 +0200

    feat(PN-14860): render pickup points on map with clustering (#136)

--- src/components/PickupPointsMap/index.tsx
+++ src/components/PickupPointsMap/index.tsx
@@ -1,9 +1,11 @@
+import { MapLibreEvent } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { Map } from "react-map-gl/maplibre";
 import { useConfig } from "src/context/config-context";
 import { RaddOperator } from "src/model";
 import MapControls from "./MapControls";
 import UserPositionController from "./UserPositionController";
+import Clusters from "./Clusters";
 
 type Props = {
   points: Array<RaddOperator>;
@@ -11,6 +13,12 @@ type Props = {
 
 const PickupPointsMap: React.FC<Props> = ({ points }) => {
   const { CLOUDFRONT_MAP_URL } = useConfig();
+
+  const handleLoad = async (event: MapLibreEvent) => {
+    const map = event.target;
+    const response = await map.loadImage("/static/images/map/base-marker.png");
+    map.addImage("base-marker", response.data);
+  };
 
   return (
     <Map
@@ -20,11 +28,14 @@ const PickupPointsMap: React.FC<Props> = ({ points }) => {
         latitude: 41.895679,
         zoom: 10,
       }}
+      minZoom={5}
+      onLoad={handleLoad}
+      reuseMaps
       style={{ height: "100%", width: "100%", position: "relative" }}
     >
       <UserPositionController points={points} />
-
       <MapControls />
+      <Clusters points={points} />
     </Map>
   );
 };
