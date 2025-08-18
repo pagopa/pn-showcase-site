5af914ee52396d6ecba4ed24021993ef07ac46f5
Alessandro Gelmi <ale.gelmi2@gmail.com>
2025-08-18 10:26:26 +0200

    feat(PN-15918): make clusters clickable (#166)

--- src/components/PickupPointsMap/index.tsx
+++ src/components/PickupPointsMap/index.tsx
@@ -1,19 +1,19 @@
 import { Typography } from "@mui/material";
+import { MAP_MARKERS } from "@utils/constants";
 import { fitMapToPoints } from "@utils/map";
-import { MapLayerMouseEvent, MapLibreEvent } from "maplibre-gl";
+import { GeoJSONSource, MapLayerMouseEvent, MapLibreEvent } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef, useState } from "react";
 import { Map, MapRef } from "react-map-gl/maplibre";
 import { useConfig } from "src/context/config-context";
+import { useIsMobile } from "src/hook/useIsMobile";
 import { useTranslation } from "src/hook/useTranslation";
 import { Coordinates, RaddOperator } from "src/model";
 import ErrorBox from "../ErrorBox";
 import Clusters from "./Clusters";
 import MapControls from "./MapControls";
-import UserPositionController from "./UserPositionController";
-import { useIsMobile } from "src/hook/useIsMobile";
 import SearchedAddressLayer from "./SearchedAddressLayer";
-import { MAP_MARKERS } from "@utils/constants";
+import UserPositionController from "./UserPositionController";
 
 type Props = {
   points: Array<RaddOperator>;
@@ -55,16 +55,18 @@ const PickupPointsMap: React.FC<Props> = ({
     }
   };
 
-  const handleMapClick = (event: MapLayerMouseEvent) => {
+  const handleMapClick = async (event: MapLayerMouseEvent) => {
     if (event.features && event.features.length > 0) {
       const feature = event.features[0];
       const map = event.target;
+      const geometry = feature.geometry as GeoJSON.Geometry & {
+        coordinates: [number, number];
+      };
 
       if (feature.layer.id === "unclustered-points") {
-        const selectedPoint = JSON.parse(feature.properties.point);
-        const geometry = feature.geometry as GeoJSON.Geometry & {
-          coordinates: [number, number];
-        };
+        const selectedPoint: RaddOperator = JSON.parse(
+          feature.properties.point
+        );
         map.flyTo({
           center: geometry.coordinates,
           zoom: 15,
@@ -72,6 +74,16 @@ const PickupPointsMap: React.FC<Props> = ({
         });
         setSelectedPoint(selectedPoint);
         toggleDialog(true, selectedPoint);
+      }
+
+      if (feature.layer.id === "cluster-points" && feature.properties.cluster) {
+        const clusterId = feature.properties.cluster_id;
+        const source: GeoJSONSource | undefined = map.getSource("stores");
+        const zoom = await source?.getClusterExpansionZoom(clusterId);
+        map.easeTo({
+          center: geometry.coordinates,
+          zoom,
+        });
       }
     }
   };
@@ -136,7 +148,7 @@ const PickupPointsMap: React.FC<Props> = ({
       }}
       minZoom={5}
       onError={() => setMapError(true)}
-      interactiveLayerIds={["unclustered-points"]}
+      interactiveLayerIds={["unclustered-points", "cluster-points"]}
       onClick={handleMapClick}
       onLoad={handleLoad}
       onMouseEnter={handleMouseEnter}
