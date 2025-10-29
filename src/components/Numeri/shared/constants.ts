51394f221c206d90d48ca78d80797c0013bf8371
Stefano Frontini <stefanofrontini75@gmail.com>
2025-10-29 10:50:25 +0100

    fix typo

--- src/components/Numeri/shared/constants.ts
+++ src/components/Numeri/shared/constants.ts
@@ -1,4 +1,4 @@
 export const curYear = new Date().getFullYear();
 export const firstYear = curYear - 2;
 export const url =
-  "https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/send/dashboard-send-sezion2.json";
+  "https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/send/dashboard-send-sezione2.json";
